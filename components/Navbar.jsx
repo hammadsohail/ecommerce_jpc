2002a33a57ab9cccf0c118b907f9067e368f4ab9
hammadsohail <muhammadhammad848@gmail.com>
2022-12-16 23:53:57 +0500

    update01

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">JSM Headphones</Link>
+        <Link href="/">Hammad Sohail EShop</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
